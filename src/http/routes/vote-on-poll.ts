4a40a853281d51e217bb8f20a29ea7b1374c9f0a
Paulo Brito <ext.paulo.brito@farfetch.com>
2024-02-08 12:30:59 +0000

    firts commit

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,76 @@
+import z from "zod"
+import { prisma } from "../../lib/prisma"
+import { FastifyInstance } from "fastify"
+import { randomUUID } from "crypto"
+import { redis } from "../../lib/redis"
+import { voting } from "../../utils/voting-pub-sub"
+
+export async function voteOnPoll(app: FastifyInstance) {
+  app.post('/polls/:pollId/votes', async (req, reply) => {
+    const votesOnPollBody = z.object({
+      pollOptionId: z.string().uuid()
+    })
+
+    const voteOnPollParams = z.object({
+      pollId: z.string().uuid()
+    })
+    const { pollId } = voteOnPollParams.parse(req.params)
+    const { pollOptionId } = votesOnPollBody.parse(req.body)
+
+    let { sessionId } = req.cookies
+
+    if (sessionId) {
+      const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+        where: {
+          sessionId_pollId: {
+            sessionId,
+            pollId
+          }
+        }
+      })
+
+      if (userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnPoll.id
+          }
+        })
+        const votes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+        voting.publish(pollId, {
+          pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+          votes: Number(votes)
+        })
+      } else if (userPreviousVoteOnPoll) {
+        return reply.status(400).send({ message: "You already voted on this poll." })
+
+      }
+    }
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.setCookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        signed: true,
+        httpOnly: true
+      })
+    }
+
+    await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId
+      }
+    })
+
+    const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes)
+    })
+
+    return reply.status(201).send()
+  })
+}
